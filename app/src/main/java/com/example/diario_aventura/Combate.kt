2bfd2ad5c305251fc7327125ed8778302d257d09
GCorbella <guillepcorbella@hotmail.com>
2023-08-24 00:06:05 +0200

    Funcionalidad para editar los usos de competencias de Combate activa.

--- app/src/main/java/com/example/diario_aventura/Combate.kt
+++ app/src/main/java/com/example/diario_aventura/Combate.kt
@@ -2,11 +2,48 @@ package com.example.diario_aventura
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
 import com.example.diario_aventura.R
 
 class Combate : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pnt_combate)
+
+        var editTextArray = arrayOf<EditText>(
+            findViewById(R.id.etxt_u_desarmado),
+            findViewById(R.id.etxt_u_dagas),
+            findViewById(R.id.etxt_u_estoques),
+            findViewById(R.id.etxt_u_latigos),
+            findViewById(R.id.etxt_u_mazas),
+            findViewById(R.id.etxt_u_hachas),
+            findViewById(R.id.etxt_u_espadas),
+            findViewById(R.id.etxt_u_lanzas),
+            findViewById(R.id.etxt_u_mazas2M),
+            findViewById(R.id.etxt_u_hachas2M),
+            findViewById(R.id.etxt_u_espadas2M),
+            findViewById(R.id.etxt_u_armasdobles),
+            findViewById(R.id.etxt_u_arcos),
+            findViewById(R.id.etxt_u_ballestas),
+            findViewById(R.id.etxt_u_arrojadizas),
+            findViewById(R.id.etxt_u_arfuegocortas),
+            findViewById(R.id.etxt_u_arfuegolargas),
+            findViewById(R.id.etxt_u_hproyectil),
+            findViewById(R.id.etxt_u_hrayo)
+        )
+
+        val btnEditarUsos = findViewById<ImageButton>(R.id.btn_editarusos_c)
+
+        btnEditarUsos.setOnClickListener {
+            // Cambia el estado de edición del EditText
+            for (eTxt in editTextArray) {
+                eTxt.isEnabled = !eTxt.isEnabled
+
+                if (!eTxt.isEnabled) {
+                    //Guardar en base de datos
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/diario_aventura/Habilidades.kt
+++ app/src/main/java/com/example/diario_aventura/Habilidades.kt
@@ -10,25 +10,25 @@ class Habilidades() : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pnt_habilidades)
 
-        var editTextArray = arrayOf(
-            findViewById<EditText>(R.id.etxt_u_agilidad),
-            findViewById<EditText>(R.id.etxt_u_artesania1),
-            findViewById<EditText>(R.id.etxt_u_artesania2),
-            findViewById<EditText>(R.id.etxt_u_atletismo),
-            findViewById<EditText>(R.id.etxt_u_avistar),
-            findViewById<EditText>(R.id.etxt_u_buscar),
-            findViewById<EditText>(R.id.etxt_u_concentracion),
-            findViewById<EditText>(R.id.etxt_u_des_manual),
-            findViewById<EditText>(R.id.etxt_u_empatia),
-            findViewById<EditText>(R.id.etxt_u_escuchar),
-            findViewById<EditText>(R.id.etxt_u_interpretar1),
-            findViewById<EditText>(R.id.etxt_u_interpretar2),
-            findViewById<EditText>(R.id.etxt_u_labia),
-            findViewById<EditText>(R.id.etxt_u_montar),
-            findViewById<EditText>(R.id.etxt_u_pilotar),
-            findViewById<EditText>(R.id.etxt_u_sanar),
-            findViewById<EditText>(R.id.etxt_u_sigilo),
-            findViewById<EditText>(R.id.etxt_u_supervivencia)
+        var editTextArray = arrayOf<EditText>(
+            findViewById(R.id.etxt_u_agilidad),
+            findViewById(R.id.etxt_u_artesania1),
+            findViewById(R.id.etxt_u_artesania2),
+            findViewById(R.id.etxt_u_atletismo),
+            findViewById(R.id.etxt_u_avistar),
+            findViewById(R.id.etxt_u_buscar),
+            findViewById(R.id.etxt_u_concentracion),
+            findViewById(R.id.etxt_u_des_manual),
+            findViewById(R.id.etxt_u_empatia),
+            findViewById(R.id.etxt_u_escuchar),
+            findViewById(R.id.etxt_u_interpretar1),
+            findViewById(R.id.etxt_u_interpretar2),
+            findViewById(R.id.etxt_u_labia),
+            findViewById(R.id.etxt_u_montar),
+            findViewById(R.id.etxt_u_pilotar),
+            findViewById(R.id.etxt_u_sanar),
+            findViewById(R.id.etxt_u_sigilo),
+            findViewById(R.id.etxt_u_supervivencia)
         )
 
         val btnEditarUsos = findViewById<ImageButton>(R.id.btn_editarusos_h)
@@ -38,7 +38,7 @@ class Habilidades() : AppCompatActivity() {
             for (eTxt in editTextArray) {
                 eTxt.isEnabled = !eTxt.isEnabled
 
-                if (eTxt.isEnabled) {
+                if (!eTxt.isEnabled) {
                     //Guardar en base de datos
                 }
             }
