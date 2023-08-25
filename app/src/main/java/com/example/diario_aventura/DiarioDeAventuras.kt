608c667bcd25d04c5cc57dc7489989dd5005760b
GCorbella <guillepcorbella@hotmail.com>
2023-08-25 21:20:09 +0200

    Desplegable de Configuracion permite seleccionar personajes en abse de Datos y establecerlos como variable global

--- app/src/main/java/com/example/diario_aventura/DiarioDeAventuras.kt
+++ app/src/main/java/com/example/diario_aventura/DiarioDeAventuras.kt
@@ -7,6 +7,10 @@ import com.example.diario_aventura.db.PersonajesDB
 class DiarioDeAventuras : Application() {
     lateinit var db : PersonajesDB // Declaración con lateinit para diferir la inicialización
 
+    companion object {
+        var personajeSeleccionadoId: Int = -1 // Valor predeterminado
+    }
+
     override fun onCreate() {
         super.onCreate()
         db = Room.databaseBuilder(
--- app/src/main/java/com/example/diario_aventura/ui/Configuracion.kt
+++ app/src/main/java/com/example/diario_aventura/ui/Configuracion.kt
@@ -2,11 +2,54 @@ package com.example.diario_aventura.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import com.example.diario_aventura.DiarioDeAventuras
 import com.example.diario_aventura.R
+import com.example.diario_aventura.db.entities.Personaje
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class Configuracion : AppCompatActivity() {
+
+    private lateinit var dspl_pj: Spinner
+    private lateinit var personajes: List<Personaje>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pnt_configuracion)
+
+        dspl_pj = findViewById(R.id.dspl_pj)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            cargarPersonajesEnDesplegable()
+        }
+
+        dspl_pj.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val personajeSeleccionado = personajes[position]
+                DiarioDeAventuras.personajeSeleccionadoId = personajeSeleccionado.id
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+        }
+    }
+
+    private suspend fun cargarPersonajesEnDesplegable() {
+        // Cargar la lista de personajes desde la base de datos
+        personajes = withContext(Dispatchers.IO) {
+            (application as DiarioDeAventuras).db.personajeDao().getPersonajesOrderedByNombre()
+        }
+
+        val personajesNombres = personajes.map { it.nombre }
+
+        val adapter = ArrayAdapter(this@Configuracion, android.R.layout.simple_spinner_item, personajesNombres)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        dspl_pj.adapter = adapter
     }
 }
